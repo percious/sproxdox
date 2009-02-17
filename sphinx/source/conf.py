c5e2f3f887ce3b942223317f1fcb7336d05710f8
Chris Perkins <chris@percious.com>
2009-02-17 01:12:50 +0000

    updated docs for 0.5.5

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -24,7 +24,7 @@ from sprox.release import __version__
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.autosummary']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
